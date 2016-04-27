856a7722215efb4a07f145334210a0ea43611b7b
FajarAH <akbaransyah@gmail.com>
2016-04-27 13:15:34 +0700

    membuat method penjumlahan dan pengurangan

--- LaporanBab5/src/Perhitungan.java
+++ LaporanBab5/src/Perhitungan.java
@@ -2,5 +2,13 @@
 public class Perhitungan {
     private static double hasil;
     
+    public static double Penjumlahan (double a, double b){
+        hasil = a + b;
+        return hasil;
+    }
+    public static double Pengurangan (double a, double b){
+        hasil = a - b;
+        return hasil;
+    }
     
 }
