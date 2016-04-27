d4eac3138535b89f94409d41d1ce6f7a22b0e8d1
FajarAH <akbaransyah@gmail.com>
2016-04-27 15:07:11 +0700

    membuat method untuk menyederhanakan pecahan

--- LaporanBab5/src/Perhitungan.java
+++ LaporanBab5/src/Perhitungan.java
@@ -18,5 +18,20 @@ public class Perhitungan {
         hasil = a / b;
         return hasil;
     }
+    public void sederhana (int pemb, int peny){
+        System.out.println("Pecahan sebelum disederhanakan\t= "+pemb+"/"+peny);
+        int pembagiPemb = pemb;
+        int pembagiPeny = peny;
+        while (pembagiPemb != pembagiPeny) {
+            if (pembagiPemb > pembagiPeny) {
+                pembagiPemb = pembagiPemb - pembagiPeny;
+            } else {
+                pembagiPeny = pembagiPeny - pembagiPemb;
+            }
+        }
+        int pembFinal = pemb / pembagiPemb;
+        int penyFinal = peny / pembagiPeny;              
+        System.out.println("Pecahan setelah disederhanakan\t= " + pembFinal + "/" + penyFinal);
     
+    }
 }
