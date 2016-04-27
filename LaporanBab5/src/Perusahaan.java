6e02d75824075ed03a439f979234d240291f5b65
FajarAH <akbaransyah@gmail.com>
2016-04-27 15:40:58 +0700

    membuat method
    method untuk menghitung total harga setiap barang

--- LaporanBab5/src/Perusahaan.java
+++ LaporanBab5/src/Perusahaan.java
@@ -3,7 +3,38 @@ public class Perusahaan {
     private final int jaketA = 100000;
     private final int jaketB = 125000;
     private final int jaketC = 175000;
+    private static int jumlahA = 0;
+    private static int jumlahB = 0;
+    private static int jumlahC = 0;
     
+    
+    
+    public int totalA(int jumlah) {
+        if (jumlah > 100) {
+            jumlahA = jumlah * (jaketA - 5000);
+        } else {
+            jumlahA = jumlah * jaketA;
+        }
+        return jumlahA;
+    }
+
+    public int totalB(int jumlah) {
+        if (jumlah > 100) {
+            jumlahB = jumlah * (jaketB - 5000);
+        } else {
+            jumlahB = jumlah * jaketB;
+        }
+        return jumlahB;
+    }
+    public int totalC(int jumlah){
+        if(jumlah>100){
+            jumlahC = jumlah * (jaketC - 15000);
+        }
+        else{
+            jumlahC = jumlah * jaketC;
+        }
+        return jumlahC;
+    }
      public void displayMessage() {
         System.out.println("==========================================");
         System.out.printf("%27s \n","CV. LABKOMDAS");
