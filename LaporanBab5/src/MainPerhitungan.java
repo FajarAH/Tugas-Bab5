f358bd6bc251c8e7b055a73b71a277c3e45f43c0
FajarAH <akbaransyah@gmail.com>
2016-04-27 14:56:43 +0700

    membuat menu pada main class

--- LaporanBab5/src/MainPerhitungan.java
+++ LaporanBab5/src/MainPerhitungan.java
@@ -1,6 +1,21 @@
 
+import java.util.Scanner;
+
+
 public class MainPerhitungan {
     public static void main(String[] args) {
+          Scanner in = new Scanner(System.in);
+        int pil;
+        Perhitungan user = new Perhitungan();
         
+            System.out.println("\t\tMesin Perhitungan");
+            System.out.println("Daftar Menu: ");
+            System.out.println("1. Penjumlahan");
+            System.out.println("2. Pengurangan");
+            System.out.println("3. Perkalian");
+            System.out.println("4. Pembagian");
+            System.out.println("5. Penyederhanaan Pecahan");
+            System.out.print("Masukkan pilihan anda\t: ");
+            pil = in.nextInt();
     }
 }
