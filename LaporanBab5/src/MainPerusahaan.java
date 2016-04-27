a69648fa90c4b609e87ca086000405db96e84b81
FajarAH <akbaransyah@gmail.com>
2016-04-27 15:53:28 +0700

    membuat switch case pada mainclass

--- LaporanBab5/src/MainPerusahaan.java
+++ LaporanBab5/src/MainPerusahaan.java
@@ -6,8 +6,28 @@ public class MainPerusahaan {
     public static void main(String[] args) {
          Scanner in = new Scanner(System.in);
         Perusahaan user = new Perusahaan();
-        int pilih;
+        int pilih,jumlah;
         user.displayMessage();
-        
+         System.out.print("Pilihan Anda\t: ");
+            pilih = in.nextInt();
+            switch (pilih) {
+                case 1:
+                    System.out.print("Banyak Jaket\t: ");
+                    jumlah = in.nextInt();
+                    user.setA(jumlah);
+                    break;
+                case 2:
+                    System.out.print("Banyak Jaket\t: ");
+                    jumlah = in.nextInt();
+                    user.setB(jumlah);
+                    break;
+                case 3:
+                    System.out.print("Banyak Jaket\t: ");
+                    jumlah = in.nextInt();
+                    user.setC(jumlah);
+                    break;
+                default:
+                    System.out.println("MAAF MENU YANG ANDA MASUKKAN TIDAK TERDAFTAR SILAHKAN COBA LAGI.");
+            }
     }
 }
